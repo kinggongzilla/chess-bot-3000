b9a7b32001e2b415a4f10af89e068913cd527528
David Hauser <hauser-david@hotmail.com>
2025-11-29 19:13:13 +0100

    250M config & sbatch

---
+++ slurm_scripts/250M_leonardo.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+#SBATCH --account=EUHPC_D18_005
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=boost_usr_prod
+#SBATCH --qos=boost_qos_lprod
+#SBATCH --time=48:00:00
+#SBATCH --chdir=/leonardo_scratch/fast/EUHPC_D18_005/david/chess-bot-3000
+#SBATCH --output=/leonardo_scratch/fast/EUHPC_D18_005/david/outputs/chess.out
+#SBATCH --error=/leonardo_scratch/fast/EUHPC_D18_005/david/outputs/chess.err
+
+source nanotron_env/bin/activate
+
+export HF_HOME="/leonardo_work/EUHPC_D18_005/david/hf-datasets-cache"
+
+# hopefully avoids timeout error after loading data shards
+export NCCL_TIMEOUT=14400000          # 4 hours in ms
+export TORCH_NCCL_HEARTBEAT_TIMEOUT_SEC=14400
+export NCCL_ASYNC_ERROR_HANDLING=1
+export NCCL_IB_TIMEOUT=23     
+
+wandb enabled
+wandb offline
+
+cd nanotron
+
+CUDA_DEVICE_MAX_CONNECTIONS=1 torchrun --nproc_per_node=1 run_train.py --config-file /leonardo_scratch/fast/EUHPC_D18_005/david/chess-bot-3000/nanotron_train_configs/250m_smollm3_chess_leonardo_jan24.yaml
